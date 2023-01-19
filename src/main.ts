8ade5c8d85c19a36c6237b990f39b1486d03b0e0
Max <maxime.michel03@protonmail.com>
2023-01-19 15:49:55 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true }); // TODO : enable only app url{ origin: 'http://localhost:3000/', credentials: true }
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Carloc Swagger')
+    .setDescription('this is the swagger of the Carloc API')
+    .setVersion('1.0')
+    .addTag('cars')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
